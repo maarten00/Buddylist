5bcc5bff02afdfb64795f579ce840ee828a951ac
Maarten Kuiper <maarten00@gmail.com>
2013-06-14 16:53:21 +0200

    New partial for userlist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to root_url, notice: "Signed up!"
     else
       render "new"
     end
   end
 
   def show
-    @blogpostings = Blogposting.all.where(:user_id => @user.id)
-    @filepostings = Fileposting.all.where(:user_id => @user.id)
+    @blogpostings = Blogposting.all.where(user_id: @user.id)
+    @filepostings = Fileposting.all.where(user_id: @user.id)
   end
 
   def index
