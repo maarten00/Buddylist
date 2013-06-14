8d4e14851e56d70e56cf555359c266978d6c42c4
Maarten Kuiper <maarten00@gmail.com>
2013-06-14 23:50:40 +0200

    Bugfix for fileposting tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Buddylist::Application.routes.draw do
   get 'blogtags/:tag', to: 'blogpostings#index', as: :blogtag
-  get 'filetags/:tag', to: 'filepostings#index', as: :audiotag
+  get 'filetags/:tag', to: 'filepostings#index', as: :filetag
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
