0b40b8c5638f56fb43566a0ffd7fe88e7127f48c
Maarten Kuiper <maarten00@gmail.com>
2013-06-17 23:35:22 +0200

    Refactoring with new rails 4 concerns

--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,21 @@ Buddylist::Application.routes.draw do
   get "sign_up" => "users#new", :as => "sign_up"
   get "accept_friendship/:id" => "friendships#accept", :as => "accept_friendship"
 
+  concern :commentable do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  concern :autocompletable do
+    get :autocomplete_tag_name, on: :collection
+  end
+
   resources :sessions
   resources :users
   resources :friendships
   resources :personalpostings
-  resources :blogpostings do
-    resources :comments, :only => [:create, :destroy]
-    get :autocomplete_tag_name, :on => :collection
-  end
-  resources :filepostings do
-    resources :comments, :only => [:create, :destroy]
-    get :autocomplete_tag_name, :on => :collection
-  end
-  resources :postings do
-    resources :comments, :only => [:create, :destroy]
-  end
+  resources :blogpostings, :concerns => [:commentable, :autocompletable]
+  resources :filepostings, :concerns => [:commentable, :autocompletable]
+  resources :postings, :concerns => [:commentable]
 
   root :to => 'postings#index'
 
