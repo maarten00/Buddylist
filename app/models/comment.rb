1c21dddcc1c1aeb3fe589b546b19519d0ffbddc9
Maarten Kuiper <maarten00@gmail.com>
2013-06-19 10:56:17 +0200

    Refactoring

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,14 +4,13 @@ class Comment < ActiveRecord::Base
   belongs_to :fileposting
 
   validates :user_id, :presence => true
-  validate :blogposting_xor_fileposting_xor_videoposting
+  validate :blogposting_xor_fileposting
   validates :content, :presence => true, :length => {:maximum => 50000}
 
   default_scope :order => 'comments.created_at asc'
 
   private
-
-  def blogposting_xor_fileposting_xor_videoposting
+  def blogposting_xor_fileposting
     if !(blogposting.blank? ^ fileposting.blank?)
       errors.add("Comment must be placed on a blog, audio or videoposting.")
     end
